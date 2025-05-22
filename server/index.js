8f1ce7b1f2de25ce4c8c817dd11f961cc6138132
Aryan Kalra <aryan.smart832@gmail.com>
2025-05-22 17:44:25 +0530

    setup backend server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
 import http from "http";
+import express from "express";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: true,
+});
+
+const PORT = process.env.PORT || 8001;
+
+io.on("connection", (socket) => {
+  socket.on("user:joined", () => {});
+});
+
+app.get("/api", (req, res) => {
+  res.send("Socket server is up and running");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running at ", PORT);
+});
